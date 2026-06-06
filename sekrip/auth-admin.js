82a48bda8b79a349c8b8da5bebc95ea275fb6a14
doopqt <dominikusapaja@gmail.com>
2026-06-06 23:02:41 +0700

    Delete sekrip directory

--- sekrip/auth-admin.js
+++
@@ -1,6 +0,0 @@
-const username = localStorage.getItem("username");
-
-if (username !== "admin") {
-    alert("Akses ditolak, halaman ini hanya untuk admin.");
-    window.location.href = "index.html";
-}
